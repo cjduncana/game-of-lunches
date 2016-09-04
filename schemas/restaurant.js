0925a35dae5ee7d55f9a23fd15cb9bc322a337e7
Christopher Duncan <cj.duncan.a@gmail.com>
2016-09-04 11:19:53 -0500

    Fix schema for empty and default values

--- schemas/restaurant.js
+++ schemas/restaurant.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 
 // TODO: When labels are fixed, reimplement them
 module.exports = Joi.object().keys({
-  id: Joi.string().optional()
+  id: Joi.string().allow('').optional()
     .description('Restaurant\'s ID')
     .example('c4928a13-8632-42f1-94e8-a6ed26342526'),
     // .label('ID'),
